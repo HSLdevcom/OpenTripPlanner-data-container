99a03d623b1e4ee34a1086915be981a3ec444673
sharhio <sade.harhio@cgi.com>
2025-01-07 13:14:18 +0200

    rename for clarity

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -4,7 +4,7 @@ module.exports = {
   id: 'waltti-alt',
   src: [
     mapSrc('WalttiTest', 'http://digitransit-proxy:8080/out/lmjadmin.mattersoft.fi/feeds/229.zip', true),
-    mapSrc('TurkuTest', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
+    mapSrc('TurkuTrunkroutes', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
   ],
   osm: ['oulu', 'southwestFinland']
 }
