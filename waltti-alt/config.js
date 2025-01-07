2e14c57c7100b6f1002de2cdad901f005de1ecce
sharhio <sade.harhio@cgi.com>
2025-01-07 13:08:21 +0200

    DT-6626 Turku test data config

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -4,6 +4,7 @@ module.exports = {
   id: 'waltti-alt',
   src: [
     mapSrc('WalttiTest', 'http://digitransit-proxy:8080/out/lmjadmin.mattersoft.fi/feeds/229.zip', true),
+    mapSrc('TurkuTest', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
   ],
-  osm: ['oulu']
+  osm: ['oulu', 'southwestFinland']
 }
