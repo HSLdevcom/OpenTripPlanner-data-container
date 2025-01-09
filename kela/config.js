f74f6add47bf39fa5eccf368224b75d9ce00c2e8
Ville Pihlava <ville.pihlava@cgi.com>
2025-01-09 11:58:19 +0200

    Enable remove-matching-route.rule for matkahuolto data in kela config.

--- kela/config.js
+++ kela/config.js
@@ -16,7 +16,7 @@ module.exports = {
     mapSrc('kela', 'https://mobility.mobility-database.fintraffic.fi/static/Kela_suuret.zip'),
     mapSrc('kela_varely', 'https://mobility.mobility-database.fintraffic.fi/static/Kela_varely.zip'),
     mapSrc('kela_waltti', 'https://mobility.mobility-database.fintraffic.fi/static/kela_waltti.zip'),
-    mapSrc('matkahuolto', mhAddress, false, ['kela/gtfs-rules/matkahuolto.rule'], { 'transfers.txt': null }),
+    mapSrc('matkahuolto', mhAddress, false, ['kela/gtfs-rules/matkahuolto.rule', 'kela/gtfs-rules/remove-matching-route.rule'], { 'transfers.txt': null }),
   ],
   osm: ['finland']
 }
