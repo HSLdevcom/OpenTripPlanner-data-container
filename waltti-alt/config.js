6b63c8e932a65789a51945cc3a2fc6703e7c5b69
sharhio <sade.harhio@cgi.com>
2025-01-30 14:46:17 +0200

    DT-6708 & DT-6710 fare data for oulu and pori alt data source

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -9,6 +9,22 @@ module.exports = {
       true,
     ),
     mapSrc('TurkuTrunkroutes', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
+    mapSrc('OULU', 'https://tvv.fra1.digitaloceanspaces.com/229.zip',
+      true,
+      undefined,
+      {
+        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
+        'fare_rules.txt': 'digitransit_fare_rules.txt',
+      },
+    ),
+    mapSrc('Pori', 'https://tvv.fra1.digitaloceanspaces.com/231.zip',
+      true,
+      undefined,
+      {
+        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
+        'fare_rules.txt': 'digitransit_fare_rules.txt',
+      },
+    ),
   ],
   osm: ['oulu', 'southFinland'],
 };
