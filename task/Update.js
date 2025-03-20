8bf67d5891c41352a6658a1e272b7a42c3a519ae
Ville Pihlava <ville.pihlava@cgi.com>
2025-03-20 15:47:17 +0200

    Extract methods from duplicate code in Update.js.

--- task/Update.js
+++ task/Update.js
@@ -28,16 +28,15 @@ function getDateString() {
   return new Date().toISOString().slice(0, -5).concat('Z').replace(/:/g, '.');
 }
 
-/**
- * This function only builds the street graph with OSM and DEM data.
- */
-async function buildStreetOnlyGraph(name) {
+async function handleSeeding() {
   if (!process.env.NOSEED) {
     process.stdout.write('Starting seeding\n');
     await start('seed');
     process.stdout.write('Seeded\n');
   }
+}
 
+async function handleOsmAndDemUpdate() {
   if (!process.env.NODEM) {
     // we track data rejections using this global variable
     global.hasFailures = false;
@@ -64,12 +63,85 @@ async function buildStreetOnlyGraph(name) {
     global.hasFailures = true;
     postSlackMessage('OSM data update failed, using previous version :boom:');
   }
+}
+
+function handleTests() {
+  if (process.env.SKIPPED_SITES === 'all' || process.env.SKIP_OTP_TESTS) {
+    process.stdout.write('Skipping all tests\n');
+  } else {
+    process.stdout.write('Test the newly built graph with OTPQA\n');
+    execFileSync('./test.sh', [], { stdio: [0, 1, 2] });
+  }
+}
+
+async function handleGtfsFallback(logFile) {
+  // testing detected routing problems
+  global.hasFailures = true;
+
+  global.failedFeeds = fs.readFileSync(logFile, 'utf8'); // comma separated list of feed ids. No newline at end!
+  fs.unlinkSync(logFile); // cleanup for local use
+
+  if (global.failedFeeds.split(',').length > MAX_GTFS_FALLBACK) {
+    updateSlackMessage(
+      'Aborting the data update because too many quality tests failed :boom:',
+    );
+    process.exit(1);
+  }
+
+  postSlackMessage(
+    `GTFS packages ${global.failedFeeds} rejected, using fallback to current data`,
+  );
+  // use seed packages for failed feeds
+  await start('gtfs:fallback');
+}
+
+function buildAndDeployDockerImages(date) {
+  process.stdout.write('Build and deploy Docker images\n');
+  execFileSync('./otp-data-server/deploy.sh', [date], {
+    stdio: [0, 1, 2],
+    env: {
+      OTP_TAG: process.env.OTP_TAG,
+      OTP_GRAPH_DIR: global.storageDirName,
+      ROUTER_NAME: process.env.ROUTER_NAME,
+      ORG: process.env.ORG,
+      DOCKER_TAG: process.env.DOCKER_TAG,
+      DOCKER_USER: process.env.DOCKER_USER,
+      DOCKER_AUTH: process.env.DOCKER_AUTH,
+    },
+  });
+  execFileSync('./opentripplanner/deploy-otp.sh', [date], {
+    stdio: [0, 1, 2],
+    env: {
+      OTP_TAG: process.env.OTP_TAG,
+      OTP_GRAPH_DIR: global.storageDirName,
+      ROUTER_NAME: process.env.ROUTER_NAME,
+      ORG: process.env.ORG,
+      DOCKER_TAG: process.env.DOCKER_TAG,
+      DOCKER_USER: process.env.DOCKER_USER,
+      DOCKER_AUTH: process.env.DOCKER_AUTH,
+    },
+  });
+}
+
+async function handleCleanup() {
+  if (!process.env.NOCLEANUP) {
+    process.stdout.write('Remove oldest data versions from storage\n');
+    await start('storage:cleanup');
+  }
+}
+
+/**
+ * This function only builds the street graph with OSM and DEM data.
+ */
+async function buildStreetOnlyGraph(name) {
+  await handleSeeding()
+
+  await handleOsmAndDemUpdate()
 
   process.stdout.write('Build street only graph\n');
   await start('router:buildStreetOnlyGraph');
 
   const date = getDateString();
-
   global.storageDirName = `osm-builds/${process.env.DOCKER_TAG}/${date}/${name}`;
 
   process.stdout.write('Uploading street graph only build data to storage\n');
@@ -97,114 +169,34 @@ async function buildStreetOnlyGraph(name) {
  * This function does the whole build.
  */
 async function buildGraph(name) {
-  if (!process.env.NOSEED) {
-    process.stdout.write('Starting seeding\n');
-    await start('seed');
-    process.stdout.write('Seeded\n');
-  }
+  await handleSeeding()
 
-  if (!process.env.NODEM) {
-    // we track data rejections using this global variable
-    global.hasFailures = false;
-    await start('dem:update');
-    if (global.hasFailures) {
-      postSlackMessage('DEM update failed, using previous version :boom:');
-    }
-  }
-
-  // OSM update is more complicated. Download often fails, so there is a retry loop,
-  // which breaks when a big enough file gets loaded
-  global.blobSizeOk = false; // ugly hack but gulp does not return any values from tasks
-  for (let i = 0; i < 3; i++) {
-    await start('osm:update');
-    if (global.blobSizeOk) {
-      break;
-    }
-    if (i < 2) {
-      // sleep 10 mins before next attempt
-      await new Promise(resolve => setTimeout(resolve, 600000));
-    }
-  }
-  if (!global.blobSizeOk) {
-    global.hasFailures = true;
-    postSlackMessage('OSM data update failed, using previous version :boom:');
-  }
+  await handleOsmAndDemUpdate()
 
   await start('gtfs:update');
 
   process.stdout.write('Build routing graph\n');
   await start('router:buildGraph');
 
-  if (process.env.SKIPPED_SITES === 'all' || process.env.SKIP_OTP_TESTS) {
-    process.stdout.write('Skipping all tests\n');
-  } else {
-    process.stdout.write('Test the newly built graph with OTPQA\n');
-    execFileSync('./test.sh', [], { stdio: [0, 1, 2] });
-  }
+  handleTests()
 
   const logFile = 'failed_feeds.txt';
   if (fs.existsSync(logFile)) {
-    // testing detected routing problems
-    global.hasFailures = true;
-
-    global.failedFeeds = fs.readFileSync(logFile, 'utf8'); // comma separated list of feed ids. No newline at end!
-    fs.unlinkSync(logFile); // cleanup for local use
-
-    if (global.failedFeeds.split(',').length > MAX_GTFS_FALLBACK) {
-      updateSlackMessage(
-        'Aborting the data update because too many quality tests failed :boom:',
-      );
-      process.exit(1);
-    }
-
-    postSlackMessage(
-      `GTFS packages ${global.failedFeeds} rejected, using fallback to current data`,
-    );
-    // use seed packages for failed feeds
-    await start('gtfs:fallback');
-
+    await handleGtfsFallback(logFile)
     // rebuild the graph
     process.stdout.write('Rebuild graph using fallback data\n');
     await start('router:buildGraph');
   }
 
   const date = getDateString();
-
   global.storageDirName = `${process.env.DOCKER_TAG}/${date}/${name}`;
 
   process.stdout.write('Uploading data to storage\n');
   await start('router:store');
 
-  process.stdout.write('Build and deploy Docker images\n');
-  execFileSync('./otp-data-server/deploy.sh', [date], {
-    stdio: [0, 1, 2],
-    env: {
-      OTP_TAG: process.env.OTP_TAG,
-      OTP_GRAPH_DIR: global.storageDirName,
-      ROUTER_NAME: process.env.ROUTER_NAME,
-      ORG: process.env.ORG,
-      DOCKER_TAG: process.env.DOCKER_TAG,
-      DOCKER_USER: process.env.DOCKER_USER,
-      DOCKER_AUTH: process.env.DOCKER_AUTH,
-    },
-  });
-  execFileSync('./opentripplanner/deploy-otp.sh', [date], {
-    stdio: [0, 1, 2],
-    env: {
-      OTP_TAG: process.env.OTP_TAG,
-      OTP_GRAPH_DIR: global.storageDirName,
-      ROUTER_NAME: process.env.ROUTER_NAME,
-      ORG: process.env.ORG,
-      DOCKER_TAG: process.env.DOCKER_TAG,
-      DOCKER_USER: process.env.DOCKER_USER,
-      DOCKER_AUTH: process.env.DOCKER_AUTH,
-    },
-  });
+  buildAndDeployDockerImages(date)
 
-  if (!process.env.NOCLEANUP) {
-    process.stdout.write('Remove oldest data versions from storage\n');
-    await start('storage:cleanup');
-  }
+  await handleCleanup()
 
   if (global.hasFailures) {
     updateSlackMessage(
@@ -219,87 +211,32 @@ async function buildGraph(name) {
  * This function builds the graph from prebuilt street graph data.
  */
 async function buildWithPrebuiltStreetGraph(name) {
-  if (!process.env.NOSEED) {
-    process.stdout.write('Starting seeding\n');
-    await start('seed');
-    process.stdout.write('Seeded\n');
-  }
+  await handleSeeding()
 
   await start('gtfs:update');
 
   process.stdout.write('Build routing graph from prebuilt street only graph\n');
   await start('router:buildWithPrebuiltStreetGraph');
 
-  if (process.env.SKIPPED_SITES === 'all' || process.env.SKIP_OTP_TESTS) {
-    process.stdout.write('Skipping all tests\n');
-  } else {
-    process.stdout.write('Test the newly built graph with OTPQA\n');
-    execFileSync('./test.sh', [], { stdio: [0, 1, 2] });
-  }
+  handleTests()
 
   const logFile = 'failed_feeds.txt';
   if (fs.existsSync(logFile)) {
-    // testing detected routing problems
-    global.hasFailures = true;
-
-    global.failedFeeds = fs.readFileSync(logFile, 'utf8'); // comma separated list of feed ids. No newline at end!
-    fs.unlinkSync(logFile); // cleanup for local use
-
-    if (global.failedFeeds.split(',').length > MAX_GTFS_FALLBACK) {
-      updateSlackMessage(
-        'Aborting the data update because too many quality tests failed :boom:',
-      );
-      process.exit(1);
-    }
-
-    postSlackMessage(
-      `GTFS packages ${global.failedFeeds} rejected, using fallback to current data`,
-    );
-    // use seed packages for failed feeds
-    await start('gtfs:fallback');
-
+    await handleGtfsFallback(logFile)
     // rebuild the graph
     process.stdout.write('Rebuild graph using fallback data\n');
     await start('router:buildWithPrebuiltStreetGraph');
   }
 
   const date = getDateString();
-
   global.storageDirName = `${process.env.DOCKER_TAG}/${date}/${name}`;
 
   process.stdout.write('Uploading data to storage\n');
   await start('router:storeForPrebuiltStreetGraphDataBuild');
 
-  process.stdout.write('Build and deploy Docker images\n');
-  execFileSync('./otp-data-server/deploy.sh', [date], {
-    stdio: [0, 1, 2],
-    env: {
-      OTP_TAG: process.env.OTP_TAG,
-      OTP_GRAPH_DIR: global.storageDirName,
-      ROUTER_NAME: process.env.ROUTER_NAME,
-      ORG: process.env.ORG,
-      DOCKER_TAG: process.env.DOCKER_TAG,
-      DOCKER_USER: process.env.DOCKER_USER,
-      DOCKER_AUTH: process.env.DOCKER_AUTH,
-    },
-  });
-  execFileSync('./opentripplanner/deploy-otp.sh', [date], {
-    stdio: [0, 1, 2],
-    env: {
-      OTP_TAG: process.env.OTP_TAG,
-      OTP_GRAPH_DIR: global.storageDirName,
-      ROUTER_NAME: process.env.ROUTER_NAME,
-      ORG: process.env.ORG,
-      DOCKER_TAG: process.env.DOCKER_TAG,
-      DOCKER_USER: process.env.DOCKER_USER,
-      DOCKER_AUTH: process.env.DOCKER_AUTH,
-    },
-  });
+  buildAndDeployDockerImages(date)
 
-  if (!process.env.NOCLEANUP) {
-    process.stdout.write('Remove oldest data versions from storage\n');
-    await start('storage:cleanup');
-  }
+  await handleCleanup()
 
   if (global.hasFailures) {
     updateSlackMessage(
