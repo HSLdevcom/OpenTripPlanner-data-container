492d68a8dce6c564dbe396d08d76852591f942a5
sharhio <sade.harhio@cgi.com>
2025-03-26 13:07:58 +0200

    Pori & Oulu data only in waltti, add Lahti test data waltti-alt, walttitest updaters in waltti-alt

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -10,18 +10,8 @@ module.exports = {
     ),
     mapSrc('TurkuTrunkroutes', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
     mapSrc(
-      'OULU',
-      'https://tvv.fra1.digitaloceanspaces.com/229.zip',
-      true,
-      undefined,
-      {
-        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
-        'fare_rules.txt': 'digitransit_fare_rules.txt',
-      },
-    ),
-    mapSrc(
-      'Pori',
-      'https://tvv.fra1.digitaloceanspaces.com/231.zip',
+      'Lahti',
+      'https://www.lsl.fi/tiedostot/testidata',
       true,
       undefined,
       {
