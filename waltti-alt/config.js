ccff9ea750e2be7f02782100c7965b737665975b
sharhio <sade.harhio@cgi.com>
2025-03-26 14:03:42 +0200

    format waltti-alt config

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -9,16 +9,10 @@ module.exports = {
       true,
     ),
     mapSrc('TurkuTrunkroutes', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
-    mapSrc(
-      'Lahti',
-      'https://www.lsl.fi/tiedostot/testidata',
-      true,
-      undefined,
-      {
-        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
-        'fare_rules.txt': 'digitransit_fare_rules.txt',
-      },
-    ),
+    mapSrc('Lahti', 'https://www.lsl.fi/tiedostot/testidata', true, undefined, {
+      'fare_attributes.txt': 'digitransit_fare_attributes.txt',
+      'fare_rules.txt': 'digitransit_fare_rules.txt',
+    }),
     mapSrc(
       'tampere',
       'https://ekstrat.tampere.fi/ekstrat/ptdata/tamperefeed_mattersoft.zip',
