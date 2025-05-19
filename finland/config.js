f21d2b84a4da6a95cba28d35af8d37a8bd9d8a71
sharhio <sade.harhio@cgi.com>
2025-05-19 16:07:37 +0300

    DT-6183 taxi data for matka testing

--- finland/config.js
+++ finland/config.js
@@ -8,6 +8,11 @@ module.exports = {
       'https://mobility.mobility-database.fintraffic.fi/static/finland_gtfs.zip',
       true,
     ),
+    mapSrc(
+      '02Taksi',
+      'https://resources.02taksi.fi/digitransit_02_taksi.zip',
+      false,
+    ),
   ],
   osm: ['finland', 'estonia'],
 };
