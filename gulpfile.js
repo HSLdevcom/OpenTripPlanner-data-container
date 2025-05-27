755a928a04cf6e1f7090ab5bcc0e559f996c752e
Vesa Meskanen <vesa@realsoft.com>
2025-05-27 08:41:32 +0300

    Revert "Keep tmp folder for debugging"
    This reverts commit 196e1d1e6cad6b9987e1beed2834c361648833b2.

--- gulpfile.js
+++ gulpfile.js
@@ -119,13 +119,18 @@ gulp.task(
 );
 
 // Add feedId to gtfs files in id dir, and moves files to directory 'test/gtfs'
-gulp.task('gtfs:id', () =>
-  gulp
-    .src(`${idDir}/*`, noBuf)
-    .pipe(extractFilesTask(['feed_info.txt']))
-    .pipe(setFeedIdTask())
-    .pipe(addFilesTask(['feed_info.txt']))
-    .pipe(gulp.dest(testGtfsDir)),
+gulp.task(
+  'gtfs:id',
+  gulp.series(
+    () =>
+      gulp
+        .src(`${idDir}/*`, noBuf)
+        .pipe(extractFilesTask(['feed_info.txt']))
+        .pipe(setFeedIdTask())
+        .pipe(addFilesTask(['feed_info.txt']))
+        .pipe(gulp.dest(testGtfsDir)),
+    () => del(tmpDir),
+  ),
 );
 
 // Runs mapFit on gtfs files if fit is enabled, or just moves files to directory 'filter'
