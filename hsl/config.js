01dce30ed245f8cd4dfdfff5962bc4b6dd475e46
Joel Lappalainen <lappalj8@gmail.com>
2025-06-05 18:01:44 +0300

    Revert "Revert taxi features"
    This reverts commit a8ebcf05eed7553d23d01626b7da072473c9d2e1.

--- hsl/config.js
+++ hsl/config.js
@@ -14,6 +14,11 @@ module.exports = {
       'HSLlautta',
       'https://mobility.mobility-database.fintraffic.fi/static/lautat_new.zip',
     ),
+    mapSrc(
+      '02Taksi',
+      'https://gtfsdata.blob.core.windows.net/hsl/02Taksi-espoo-test-gtfs.zip',
+      false,
+    ),
     // src('Sipoo', 'https://koontikartta.navici.com/tiedostot/rae/sipoon_kunta_sibbo_kommun.zip')
   ],
   osm: ['hsl'],
