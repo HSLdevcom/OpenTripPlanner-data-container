2eef0c6ab6a1ea2c4981b3f39deaa5594e4d269b
sharhio <sade.harhio@cgi.com>
2025-10-22 09:14:43 +0300

    AB#268 update hsl data url

--- hsl/config.js
+++ hsl/config.js
@@ -5,7 +5,7 @@ module.exports = {
   src: [
     mapSrc(
       'HSL',
-      'https://infopalvelut.storage.hsldev.com/gtfs/hsl.zip',
+      'https://infopalvelut.storage.hsldev.com/gtfs/hsl_google_transit.zip',
       false,
       undefined,
       { 'trips.txt': 'trips2.txt' },
