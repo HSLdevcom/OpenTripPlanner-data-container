f947331a351f209ce5e3842423ded183a99a0284
Vesa Meskanen <vesa@realsoft.com>
2025-11-04 12:21:51 +0200

    Use standard waltti dev routing for Lahti next dev

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -10,19 +10,6 @@ module.exports = {
     ),
     mapSrc('TurkuTrunkroutes', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
     mapSrc(
-      'Lahti',
-      'https://www.lsl.fi/tiedostot/testidata',
-      true,
-      undefined,
-      {
-        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
-        'fare_rules.txt': 'digitransit_fare_rules.txt',
-      },
-      {
-        insecureHTTPParser: true,
-      },
-    ),
-    mapSrc(
       'tampere',
       'https://ekstrat.tampere.fi/ekstrat/ptdata/tamperefeed_mattersoft.zip',
     ),
