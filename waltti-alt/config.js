da5c23f43f3b5899587fff270c53c5ef9893543a
tuomasEnterprise <tuomas.lindell@cgi.com>
2025-12-23 09:51:14 +0200

    AB#354 Enable Jyvaskyla occupancy status

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -3,11 +3,6 @@ const { mapSrc } = require('../util');
 module.exports = {
   id: 'waltti-alt',
   src: [
-    mapSrc(
-      'WalttiTest',
-      'http://digitransit-proxy:8080/out/lmjadmin.mattersoft.fi/feeds/229.zip',
-      true,
-    ),
     mapSrc('TurkuTrunkroutes', 'http://data-test.foli.fi/gtfs/gtfs.zip', true),
     mapSrc(
       'tampere',
@@ -30,6 +25,16 @@ module.exports = {
           'Accept-Encoding': 'gzip',
           'Digitraffic-User': 'Digitransit/OTP-dataloading',
         },
+      },
+    ),
+    mapSrc(
+      'LINKKI',
+      'https://tvv.fra1.digitaloceanspaces.com/209.zip',
+      true,
+      undefined,
+      {
+        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
+        'fare_rules.txt': 'digitransit_fare_rules.txt',
       },
     ),
   ],
