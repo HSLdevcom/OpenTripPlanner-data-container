6d9e8a012f1ac4997f92ce7c1144a29d0a5a5f9b
Vesa Meskanen <vesa@realsoft.com>
2026-03-11 09:58:20 +0200

    Revert "Load finland OSM from geofabrik"
    This reverts commit dc15577a34ae3ce067fc7c973980994db3377fe9.

--- config.js
+++ config.js
@@ -54,7 +54,8 @@ const extraOSM =
 
 const osm = {
   estonia: 'https://download.geofabrik.de/europe/estonia-latest.osm.pbf',
-  finland: 'https://download.geofabrik.de/europe/finland-latest.osm.pbf',
+  finland:
+    'https://karttapalvelu.storage.hsldev.com/finland.osm/finland.osm.pbf',
   hsl: 'https://karttapalvelu.storage.hsldev.com/hsl.osm/hsl.osm.pbf',
   kajaani:
     'https://karttapalvelu.storage.hsldev.com/waltti.osm/kajaani.osm.pbf',
