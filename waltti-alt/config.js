c2cb5988ef312c4fe11a2762617ef60d7e12d162
Ville Pihlava <ville.pihlava@cgi.com>
2026-03-25 17:14:01 +0200

    Remove flex test configurations.

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -27,11 +27,6 @@ module.exports = {
         },
       },
     ),
-    mapSrc(
-      '02Taksi',
-      'https://resources.02taksi.fi/digitransit_02_taksi.zip',
-      false,
-    ),
   ],
   osm: ['oulu', 'southFinland'],
 };
