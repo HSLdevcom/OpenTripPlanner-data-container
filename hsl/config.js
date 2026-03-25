fd7036b258fcf25676ca4d6eea06aecf75057d6b
Ville Pihlava <ville.pihlava@cgi.com>
2026-03-25 17:14:01 +0200

    Remove flex test configurations.

--- hsl/config.js
+++ hsl/config.js
@@ -10,11 +10,6 @@ module.exports = {
       undefined,
       { 'trips.txt': 'trips2.txt' },
     ),
-    mapSrc(
-      'Uber',
-      'https://gtfsdata.blob.core.windows.net/hsl/ubertaksi-gtfs.zip',
-      false,
-    ),
     /*
     mapSrc(
       'HSLlautta',
