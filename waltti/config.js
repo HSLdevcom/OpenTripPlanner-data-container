5220126066b27f1dbcd918c0b5fd80e764050f2a
Heikki Vuorinen <hjvuor@icloud.com>
2026-03-26 12:01:16 +0200

    Revert "Reapply "AB#352 New kuopio static gtfs""
    This reverts commit bc8348bfd2cac9d5e88fbcc18f457076e0e2ae67.

--- waltti/config.js
+++ waltti/config.js
@@ -49,13 +49,7 @@ module.exports = {
     ),
     mapSrc(
       'Kuopio',
-      'https://tvv.fra1.digitaloceanspaces.com/221.zip',
-      true,
-      undefined,
-      {
-        'fare_attributes.txt': 'digitransit_fare_attributes.txt',
-        'fare_rules.txt': 'digitransit_fare_rules.txt',
-      },
+      'https://karttapalvelu.kuopio.fi/google_transit/google_transit.zip',
     ),
     mapSrc(
       'OULU',
