f78296e18d14f7cdedd6309802e6322ed75c68ff
Teemu Kalvas <teemu.kalvas@hsl.fi>
2026-04-29 13:52:59 +0300

    prettier style fix

--- task/BuildOTPGraph.js
+++ task/BuildOTPGraph.js
@@ -80,12 +80,12 @@ const packData = function (commit, router) {
       zipWithGlobIntoDir(
         `${path}/router-${router.id}.zip`,
         [
-	  `${path}/*-gtfs.zip`,
-	  `${path}/*-netex.zip`,
-	  `${path}/*.json`,
-	  ...osmFiles,
-	  `${path}/*.tif`,
-	],
+          `${path}/*-gtfs.zip`,
+          `${path}/*-netex.zip`,
+          `${path}/*.json`,
+          ...osmFiles,
+          `${path}/*.tif`,
+        ],
         `router-${router.id}`,
       )
     ) {
