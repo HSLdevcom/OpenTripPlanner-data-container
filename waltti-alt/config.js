c19c7efdc8393413b0796c10276ee993f47419f6
Vesa Meskanen <vesa@realsoft.com>
2026-05-29 14:20:15 +0300

    New walttitest data

--- waltti-alt/config.js
+++ waltti-alt/config.js
@@ -34,7 +34,7 @@ module.exports = {
     ),
     mapSrc(
       'WalttiTest',
-      'https://tvv.fra1.digitaloceanspaces.com/229.zip',
+      'https://github.com/tvv-lippu-ja-maksujarjestelma-oy/waltti-digitransit-config-data/blob/main/GTFStestireittiopas.zip',
       'true',
     ),
   ],
