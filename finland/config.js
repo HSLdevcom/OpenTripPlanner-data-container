e3ea6b56e3316edd52ab3fa90bef856361f93ba7
Teemu Kalvas <teemu.kalvas@hsl.fi>
2026-06-23 10:45:35 +0300

    prettier

--- finland/config.js
+++ finland/config.js
@@ -42,8 +42,8 @@ module.exports = {
     {
       id: 'CAR_FERRIES',
       url: 'https://schedules.finferries.fi/export/netex.zip',
-      groupFilePattern: '().*\\.xml'
-    }
+      groupFilePattern: '().*\\.xml',
+    },
   ],
   osm: ['finland', 'estonia'],
 };
