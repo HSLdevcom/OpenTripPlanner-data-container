c09127889da44e68121d707aeed0c59292767b36
Ville Pihlava <ville.pihlava@cgi.com>
2026-08-11 18:06:24 +0300

    Clean up 'config.js'.

--- config.js
+++ config.js
@@ -17,33 +17,29 @@ const extraSrc =
 
 const SPLIT_BUILD_TYPE = process.env.SPLIT_BUILD_TYPE || 'NO_SPLIT_BUILD';
 
-const usedSrc = [];
-
-// override source values if they are defined in extraSrc
-const rt = router;
-const sources = rt.gtfs;
-for (let j = sources.length - 1; j >= 0; j--) {
-  const src = sources[j];
+// override gtfs entries that are defined in extraSrc
+const overriddenGtfsIds = [];
+for (let j = router.gtfs.length - 1; j >= 0; j--) {
+  const src = router.gtfs[j];
   const id = src.id;
   if (extraSrc[id]) {
-    usedSrc.push(id);
+    overriddenGtfsIds.push(id);
     if (extraSrc[id].remove) {
-      sources.splice(j, 1);
+      router.gtfs.splice(j, 1);
       continue;
     }
-    sources[j] = { ...src, ...extraSrc[id] };
+    router.gtfs[j] = { ...src, ...extraSrc[id] };
   }
-  sources[j].config = rt;
 }
 
 // Go through extraSrc keys to find keys that don't already exist in gtfs and add those as new entries
 Object.keys(extraSrc).forEach(id => {
-  if (!usedSrc.includes(id)) {
+  if (!overriddenGtfsIds.includes(id)) {
     router.gtfs.push({ ...extraSrc[id], id });
   }
 });
 
-// create id->src-entry map
+// create id->gtfs-entry map
 const gtfsMap = {};
 router.gtfs.forEach(src => {
   gtfsMap[src.id] = src;
